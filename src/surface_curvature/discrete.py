609e1f0bab501a46680e0d51bd6f4614d021ebdd
phcreery <phcreery@gmail.com>
2023-07-06 20:27:50 -0500

    remove redundant code

--- src/surface_curvature/discrete.py
+++ src/surface_curvature/discrete.py
@@ -3,12 +3,20 @@ import numpy as np
 # https://stackoverflow.com/questions/11317579/surface-curvature-matlab-equivalent-in-python
 
 
-def mean_curvature_orthodiscrete_monge(Z):
+def mean_curvature_orthodiscrete_monge(Z: np.array):
     """
     X is a 2D array, Y is a 2D array, and Z is a 2D array. If you have an unstructured
     cloudpoint, with a single matrix shaped Nx3 (the first column being X, the second
     being Y and the third being Z) then you can't apply this matlab function.
     This assumes that your data points are 1 unit apart.
+
+    The matrix Z is a 2D array with vertices arranged in the mesh form:
+
+    O--O--O--O
+    |  |  |  |
+    O--O--O--O
+    |  |  |  |
+    O--O--O--O
     """
     # https://stackoverflow.com/questions/11317579/surface-curvature-matlab-equivalent-in-python
     Zy, Zx = np.gradient(Z)
@@ -21,7 +29,7 @@ def mean_curvature_orthodiscrete_monge(Z):
     return H
 
 
-def gaussian_curvature_orthodiscrete_monge(Z):
+def gaussian_curvature_orthodiscrete_monge(Z: np.array):
     """
     Z is a 2D array
     The curvature in general is measured as 1/r, where r is the radius of an inscribed
@@ -36,7 +44,8 @@ def gaussian_curvature_orthodiscrete_monge(Z):
     return K
 
 
-def surfature_orthodiscrete_parametric(X, Y, Z):
+def surfature_orthodiscrete_parametric(X: np.array, Y: np.array, Z: np.array):
+    # where X, Y, Z matrices have a shape (lr+1,lb+1)
     # https://github.com/sujithTSR/surface-curvature/blob/master/surface.py
 
     (lr, lb) = X.shape
@@ -101,79 +110,6 @@ def surfature_orthodiscrete_parametric(X, Y, Z):
     H = (E * N + G * L - 2 * F * M) / (2 * (E * G - F**2))
     # H = np.reshape(H, lr * lb)
     H = np.reshape(H, (lr, lb))
-
-    # #% Principal Curvatures
-    k1 = H + np.sqrt(H**2 - K)
-    k2 = H - np.sqrt(H**2 - K)
-
-    return K, H, k1, k2  # , k1vec, k2vec
-
-
-def surfature_orthodiscrete_parametric2(X, Y, Z):
-    # where X, Y, Z matrices have a shape (lr+1,lb+1)
-    (lr, lb) = X.shape
-    lr = lr - 1
-    lb = lb - 1
-
-    # First Derivatives
-    Xv, Xu = np.gradient(X)
-    Yv, Yu = np.gradient(Y)
-    Zv, Zu = np.gradient(Z)
-
-    # Second Derivatives
-    Xuv, Xuu = np.gradient(Xu)
-    Yuv, Yuu = np.gradient(Yu)
-    Zuv, Zuu = np.gradient(Zu)
-
-    Xvv, Xuv = np.gradient(Xv)
-    Yvv, Yuv = np.gradient(Yv)
-    Zvv, Zuv = np.gradient(Zv)
-
-    # Reshape to 1D vectors
-    nrow = (lr + 1) * (lb + 1)  # total number of rows after reshaping
-    Xu = Xu.reshape(nrow, 1)
-    Yu = Yu.reshape(nrow, 1)
-    Zu = Zu.reshape(nrow, 1)
-    Xv = Xv.reshape(nrow, 1)
-    Yv = Yv.reshape(nrow, 1)
-    Zv = Zv.reshape(nrow, 1)
-    Xuu = Xuu.reshape(nrow, 1)
-    Yuu = Yuu.reshape(nrow, 1)
-    Zuu = Zuu.reshape(nrow, 1)
-    Xuv = Xuv.reshape(nrow, 1)
-    Yuv = Yuv.reshape(nrow, 1)
-    Zuv = Zuv.reshape(nrow, 1)
-    Xvv = Xvv.reshape(nrow, 1)
-    Yvv = Yvv.reshape(nrow, 1)
-    Zvv = Zvv.reshape(nrow, 1)
-
-    Xu = np.c_[Xu, Yu, Zu]
-    Xv = np.c_[Xv, Yv, Zv]
-    Xuu = np.c_[Xuu, Yuu, Zuu]
-    Xuv = np.c_[Xuv, Yuv, Zuv]
-    Xvv = np.c_[Xvv, Yvv, Zvv]
-
-    # % First fundamental Coeffecients of the surface (E,F,G)
-    E = np.einsum("ij,ij->i", Xu, Xu)
-    F = np.einsum("ij,ij->i", Xu, Xv)
-    G = np.einsum("ij,ij->i", Xv, Xv)
-
-    m = np.cross(Xu, Xv, axisa=1, axisb=1)
-    p = np.sqrt(np.einsum("ij,ij->i", m, m))
-    n = m / np.c_[p, p, p]
-
-    # % Second fundamental Coeffecients of the surface (L,M,N)
-    L = np.einsum("ij,ij->i", Xuu, n)
-    M = np.einsum("ij,ij->i", Xuv, n)
-    N = np.einsum("ij,ij->i", Xvv, n)
-
-    # % Gaussian Curvature
-    K = (L * N - M**2) / (E * G - F**2)
-    K = K.reshape(lr + 1, lb + 1)
-
-    # % Mean Curvature
-    H = (E * N + G * L - 2 * F * M) / (2 * (E * G - F**2))
-    H = H.reshape(lr + 1, lb + 1)
 
     # #% Principal Curvatures
     k1 = H + np.sqrt(H**2 - K)
